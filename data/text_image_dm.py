097bc80a63678e61eb5bd82265e23ffe8fa0f3cf
iranroman <iran@ccrma.stanford.edu>
2022-05-03 19:43:20 -0400

    update dataloader for epic kitchens

--- data/text_image_dm.py
+++ data/text_image_dm.py
@@ -6,47 +6,47 @@ import PIL
 import argparse
 import clip
 import torch
+import csv
 
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms as T
+from torchvision.io import VideoReader
 from pytorch_lightning import LightningDataModule
+
 
 
 class TextImageDataset(Dataset):
     def __init__(self,
                  folder: str,
+                 annotation_file: str,
                  image_size=224,
                  resize_ratio=0.75,
                  shuffle=False,
-                 custom_tokenizer=False
+                 custom_tokenizer=False,
+                 frame_rate = 2,
                  ):
         """Create a text image dataset from a directory with congruent text and image names.
 
         Args:
             folder (str): Folder containing images and text files matched by their paths' respective "stem"
+            annotation_file (str): file containing the action annotations 
             image_size (int, optional): The size of outputted images. Defaults to 224.
             resize_ratio (float, optional): Minimum percentage of image contained by resize. Defaults to 0.75.
             shuffle (bool, optional): Whether or not to have shuffling behavior during sampling. Defaults to False.
             custom_tokenizer (bool, optional): Whether or not there is a custom tokenizer. Defaults to False.
+            frame_rate (int): the desired number of frames used per second. Defaults to 2
         """
         super().__init__()
         self.shuffle = shuffle
+        self.frame_rate = frame_rate 
         path = Path(folder)
 
-        text_files = [*path.glob('**/*.txt')]
-        image_files = [
-            *path.glob('**/*.png'), *path.glob('**/*.jpg'),
-            *path.glob('**/*.jpeg'), *path.glob('**/*.bmp')
-        ]
+        self.video_files = [*path.glob('**/*.MP4')]
+        self.total_frames_per_video =  {video:np.floor(VideoReader(video,'video').get_metadata()['video']['duration'])*self.frame_rate for video in self.video_files}
+        self.total_frames = np.sum(list(self.total_frames_per_video.values())) * self.frame_rate
+        self.cumsum_of_frames_per_video = np.cumsum(self.total_frames_per_video)
 
-        text_files = {text_file.stem: text_file for text_file in text_files}
-        image_files = {image_file.stem: image_file for image_file in image_files}
-
-        keys = (image_files.keys() & text_files.keys())
-
-        self.keys = list(keys)
-        self.text_files = {k: v for k, v in text_files.items() if k in keys}
-        self.image_files = {k: v for k, v in image_files.items() if k in keys}
+        # the image pre-processing
         self.resize_ratio = resize_ratio
         self.image_transform = T.Compose([
             T.Lambda(self.fix_img),
@@ -54,14 +54,26 @@ class TextImageDataset(Dataset):
                                 scale=(self.resize_ratio, 1.),
                                 ratio=(1., 1.)),
             T.ToTensor(),
-            T.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
+            T.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)) # why are these values hard-coded?
         ])
+
+        # the tokenizer used for text
         self.custom_tokenizer = custom_tokenizer
 
+        # the actions and timestamps per video
+        self.timed_actions = load_timestamped_actions(annotation_file)
+
     def __len__(self):
-        return len(self.keys)
-    
-    def fix_img(self, img):
+        return self.total_frames
+
+    def load_timestamped_actions(self, folder):
+        video_ids = [os.path.basename(f).stem for f in ]
+        with open(folder) as f:
+            reader = csv.reader(f, delimiter=',')
+            timed_actions = [[row[4], row[5], row[8] for row in reader if row[2]==video_id] for video_id in video_ids]
+        return timed_actions
+
+    def fix_img(self, img): # channel conversion if needed
         return img.convert('RGB') if img.mode != 'RGB' else img
 
     def random_sample(self):
@@ -78,15 +90,23 @@ class TextImageDataset(Dataset):
         return self.sequential_sample(ind=ind)
 
     def __getitem__(self, ind):
-        key = self.keys[ind]
+        video_idx = np.argwhere(self.cumsum_of_frames_per_video > ind)[0] if ind > self.cumsum_of_frames_per_video[0] else 0
+        video_id = self.video_files[video_idx]
+    
+        frames_modulus = ind - self.cumsum_of_frames_per_video[video_idx-1] if ind > self.cumsum_of_frames_per_video[0] else ind
+        time = frames_modulus / self.frame_rate + np.random.uniform(low=-0.1, high=1.0)
+    
+        # load the video
+        video = VideoReader(video_path, 'video')
+        video.set_current_stream("video")
+        
+        frame = video.seek(time).next()
+        image = frame['data']
+        timestamp = frame['pts']
 
-        text_file = self.text_files[key]
-        image_file = self.image_files[key]
-
-        descriptions = text_file.read_text().split('\n')
-        descriptions = list(filter(lambda t: len(t) > 0, descriptions))
-        try:
-            description = choice(descriptions)
+        # find the action in the timestamp
+        descriptions = [a for (st,et,a) in self.timed_actions[video_idx] if time > st and time < et]
+        description = choice(descriptions) if len(descriptions) > 0 else 'unspecified'
         except IndexError as zero_captions_in_file_ex:
             print(f"An exception occurred trying to load file {text_file}.")
             print(f"Skipping index {ind}")
@@ -95,7 +115,7 @@ class TextImageDataset(Dataset):
         tokenized_text = description if self.custom_tokenizer else clip.tokenize(description)[0]
 
         try:
-            image_tensor = self.image_transform(PIL.Image.open(image_file))
+            image_tensor = self.image_transform(frame)
         except (PIL.UnidentifiedImageError, OSError) as corrupt_image_exceptions:
             print(f"An exception occurred trying to load file {image_file}.")
             print(f"Skipping index {ind}")
